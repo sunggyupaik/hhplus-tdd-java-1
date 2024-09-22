71d5b1d6bd186978c0061249e831844fb509a5d7
sunggyupaik <melon9751@naver.com>
2024-09-22 22:53:59 +0900

    [test] 존재하지 않는 유저 포인트 조회를 테스트하라
    주어진 유저 식별자에 해당하는 유저 포인트가 없으면
    포인트가 0인 유저 포인트를 반환하는 테스트를 추가합니다.

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -12,7 +12,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class PointServiceTest {
     private static final long EXISTED_USER_ID = 1L;
+    private static final long NOT_EXISTED_USER_ID = 99L;
     private static final long POINT_3000 = 3000L;
+    private static final long POINT_0 = 0L;
     private static final long SYSTEM_CURRENT_TIME_MILLIS = 3000L;
 
     private UserPoint userPoint;
@@ -37,5 +39,14 @@ class PointServiceTest {
 
         assertThat(detailedUserPoint.id()).isEqualTo(EXISTED_USER_ID);
         assertThat(detailedUserPoint.point()).isEqualTo(POINT_3000);
+    }
+
+    @Test
+    @DisplayName("주어진 유저 식별자에 해당하는 유저 포인트가 없으면 포인트가 0인 유저 포인트를 반환한다")
+    void detailUserPointWithNotExistedId() {
+        UserPoint emptyUserPoint = pointService.detailUserPoint(NOT_EXISTED_USER_ID);
+
+        assertThat(emptyUserPoint.id()).isEqualTo(NOT_EXISTED_USER_ID);
+        assertThat(emptyUserPoint.point()).isEqualTo(POINT_0);
     }
 }
